b516ae6cc962606137d05122d5099709d95b8c5e
A Certain Existence <a.certain.existence@gmail.com>
2026-02-28 12:10:00 +0800

    Fix duplicated exceeded expectations texts being thrown with masterful construction gene

--- 1.6/Source/Harmony/Frame_CompleteConstruction.cs
+++ 1.6/Source/Harmony/Frame_CompleteConstruction.cs
@@ -2,47 +2,18 @@
 using RimWorld;
 using System.Collections.Generic;
 using Verse;
-using Verse.Noise;
 
 namespace VanillaQuestsExpandedAncients
 {
-
-
-    [HarmonyPatch(typeof(Frame))]
-    [HarmonyPatch("CompleteConstruction")]
+    [HarmonyPatch(typeof(Frame), nameof(Frame.CompleteConstruction))]
     public static class VanillaQuestsExpandedAncients_Frame_CompleteConstruction_Patch
     {
-        public static Pawn crafter;
-
-        [HarmonyPrefix]
-        static void StoreCrafter(Pawn worker)
+        static void Postfix(Frame __instance, Pawn worker)
         {
-            crafter = worker;
-
-        }
-
-        [HarmonyPostfix]
-        static void RemoveCrafter()
-        {
-            crafter = null;
-
-        }
-    }
-
-    [HarmonyPatch(typeof(ThingMaker))]
-    [HarmonyPatch("MakeThing")]
-    public static class VanillaQuestsExpandedAncients_ThingMaker_MakeThing_Patch
-    {
-
-
-        [HarmonyPostfix]
-        static void HandleCraftModifications(ThingDef def, Thing __result)
-        {
-           if(Rand.Chance(0.5f) && VanillaQuestsExpandedAncients_Frame_CompleteConstruction_Patch.crafter?.genes?.HasActiveGene(InternalDefOf.VQEA_MasterfulConstruction) == true)
+            if (Rand.Chance(0.5f) && worker?.genes?.HasActiveGene(InternalDefOf.VQEA_MasterfulConstruction) == true)
             {
-                Pawn crafter = VanillaQuestsExpandedAncients_Frame_CompleteConstruction_Patch.crafter;
-                List<ThingDefCountClass> costlist = __result.CostListAdjusted();
-               
+                List<ThingDefCountClass> costlist = __instance.TotalMaterialCost();
+
                 foreach (ThingDefCountClass ingredientCount in costlist)
                 {
                     ThingDef stuff = ingredientCount.stuff;
@@ -52,16 +23,12 @@ namespace VanillaQuestsExpandedAncients
                     {
                         newProduct.stackCount = 1;
                     }
-                    GenSpawn.Spawn(newProduct, crafter.Position, crafter.Map);
-                    
+                    GenSpawn.Spawn(newProduct, worker.Position, worker.Map);
+
                 }
-                MoteMaker.ThrowText(crafter.Position.ToVector3(), crafter.Map, "VQEA_TextMote_ExceededExpectations".Translate(), 6f);
+
+                MoteMaker.ThrowText(worker.Position.ToVector3(), worker.Map, "VQEA_TextMote_ExceededExpectations".Translate(), 6f);
             }
-
         }
-
-
     }
-
-
 }
